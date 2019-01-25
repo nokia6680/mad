8e3b5a8184ead7b2f8b2eda1040b2d2a76941f38
nokia6680 <artur.ballod@gmail.com>
2019-01-25 17:28:11 +0200

    #4 - info section

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
-$(function() {
-  $( "#js-slider" ).slider({
-    value:400,
-    min: 0,
-    max: 800,
-    step: 5
+var serviceList = document.getElementById('service-list');
+  serviceList.style.display="none";
+   $('.nav-list__item--service').mouseover(function() {
+     serviceList.style.display="flex";
+  })
+  .mouseout(function() {
+    $(serviceList).mouseover(function() {
+    serviceList.style.display="flex";
+    })
+    .mouseout(function() {
+     serviceList.style.display="none";
+    });
   });
-});
 
 
-(function() {
-  var d = document,
-      c = d.getElementsByClassName('about-form__tech')[0],
-      t = d.getElementsByClassName('about-form__textarea')[0],
-      f = d.createTextNode('');
-
-  c.appendChild(f);
-
-  function updateSize() {
-     f.nodeValue = t.value + '\n';
-  }
-
-  t.onkeypress = t.onkeyup = t.onchange = updateSize;
-
-  updateSize();
-})();
+  var goodsList = document.getElementById('goods-list');
+    goodsList.style.display="none";
+     $('.nav-list__item--goods').mouseover(function() {
+       goodsList.style.display="flex";
+    })
+    .mouseout(function() {
+      $(goodsList).mouseover(function() {
+      goodsList.style.display="flex";
+      })
+      .mouseout(function() {
+       goodsList.style.display="none";
+      });
+    });
