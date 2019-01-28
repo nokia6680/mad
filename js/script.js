29f3d0e81ea9383d7bdc1e5770856c71d8b04482
nokia6680 <artur.ballod@gmail.com>
2019-01-28 02:06:52 +0200

    #prefinal-2

--- js/script.js
+++ js/script.js
@@ -1,62 +1,50 @@
-var serviceList = document.getElementById('service-list');
-serviceList.style.display = "none";
-$('.nav-list__item--service').mouseover(function() {
-    serviceList.style.display = "flex";
-  })
-  .mouseout(function() {
-    $(serviceList).mouseover(function() {
-        serviceList.style.display = "flex";
-      })
-      .mouseout(function() {
-        serviceList.style.display = "none";
-      });
-  });
+$(document).on("mouseover", ".nav-list__item", function() {
+  var numberIndex = $(this).index();
 
+  if (!$(this).is("nav-list__item--active")) {
+    $(".nav-list__item").removeClass("nav-list__item--active");
+    $(".lower-menu__list").removeClass("lower-menu__list--active");
 
-var goodsList = document.getElementById('goods-list');
-goodsList.style.display = "none";
-$('.nav-list__item--goods').mouseover(function() {
-    goodsList.style.display = "flex";
-  })
-  .mouseout(function() {
-    $(goodsList).mouseover(function() {
-        goodsList.style.display = "flex";
-      })
-      .mouseout(function() {
-        goodsList.style.display = "none";
-      });
-  });
+    $(this).addClass("nav-list__item--active");
+    $(".lower-menu").find("ul:eq(" + numberIndex + ")").addClass("lower-menu__list--active");
 
-  $(document).on("click", ".faq-list__item", function() {
-  	var numberIndex = $(this).index();
+    var listItemHeight = $(".lower-menu")
+      .find("ul:eq(" + numberIndex + ")")
+      .innerHeight();
+    $(".lower-menu").height(listItemHeight + "px");
+  }
+});
 
-  	if (!$(this).is("faq-list__item--active")) {
-  		$(".faq-list__item").removeClass("faq-list__item--active");
-  		$(".faq-answer__tab").removeClass("faq-answer__tab--active");
+$(document).on("click", ".faq-list__item", function() {
+  var numberIndex = $(this).index();
 
-  		$(this).addClass("faq-list__item--active");
-  		$(".faq-answer").find("li:eq(" + numberIndex + ")").addClass("faq-answer__tab--active");
+  if (!$(this).is("faq-list__item--active")) {
+    $(".faq-list__item").removeClass("faq-list__item--active");
+    $(".faq-answer__tab").removeClass("faq-answer__tab--active");
 
-  		var listItemHeight = $(".faq-answer")
-  			.find("li:eq(" + numberIndex + ")")
-  			.innerHeight();
-  		$(".faq-answer").height(listItemHeight + "px");
-  	}
-  });
+    $(this).addClass("faq-list__item--active");
+    $(".faq-answer").find("li:eq(" + numberIndex + ")").addClass("faq-answer__tab--active");
 
-  $(document).on("click", ".pattern-list__item", function() {
-    var numberIndex = $(this).index();
+    var listItemHeight = $(".faq-answer")
+      .find("li:eq(" + numberIndex + ")")
+      .innerHeight();
+    $(".faq-answer").height(listItemHeight + "px");
+  }
+});
 
-    if (!$(this).is("pattern-list__item--active")) {
-      $(".pattern-list__item").removeClass("pattern-list__item--active");
-      $(".pattern-links__list").removeClass("pattern-links__list--active");
+$(document).on("click", ".pattern-list__item", function() {
+  var numberIndex = $(this).index();
 
-      $(this).addClass("pattern-list__item--active");
-      $(".pattern-links").find("ul:eq(" + numberIndex + ")").addClass("pattern-links__list--active");
+  if (!$(this).is("pattern-list__item--active")) {
+    $(".pattern-list__item").removeClass("pattern-list__item--active");
+    $(".pattern-links__list").removeClass("pattern-links__list--active");
 
-      var listItemHeight = $(".pattern-links")
-        .find("ul:eq(" + numberIndex + ")")
-        .innerHeight();
-      $(".pattern-links").height(listItemHeight + "px");
-    }
-  });
+    $(this).addClass("pattern-list__item--active");
+    $(".pattern-links").find("ul:eq(" + numberIndex + ")").addClass("pattern-links__list--active");
+
+    var listItemHeight = $(".pattern-links")
+      .find("ul:eq(" + numberIndex + ")")
+      .innerHeight();
+    $(".pattern-links").height(listItemHeight + "px");
+  }
+});
