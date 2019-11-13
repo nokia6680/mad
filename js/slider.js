f9d96ba356bf2b9d91549f35b549ce81cab12aad
nokia6680 <zenitfan@yandex.ru>
2019-11-13 22:03:49 +0200

    ###mad

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: '.slide-next',
+    prevEl: '.slide-prev',
+  },
+});
